49a5eb354415b5eb9966ad0f43d0d4242e88045f
Kamrul Hasan <kakamrul2000@gmail.com>
2025-12-22 13:16:02 +0600

    add_image_appsTexts

---
+++ lib/constants/app_texts.dart
@@ -0,0 +1,15 @@
+class AppTexts {
+  static const title1 = 'Discover the world, one journey at a time.';
+  static const title2 = 'Explore new horizones, one step at a time.';
+  static const title3 = 'See the beauty, one journey at a time.';
+
+  static const desc1 =
+      'From hidden gems to iconic distinations, we make travel simple, inspiring, and unforgettable.';
+  static const desc2 =
+      "Every trip holds a story waiting to be lived. Let us guide you.";
+  static const desc3 =
+      "Travel made simple and exciting—discover places you’ll love.";
+
+  static const skip = 'SKIP';
+  static const next = 'Next';
+}
---
+++ lib/networks/image_urls.dart
@@ -0,0 +1,10 @@
+class ImageUrls {
+  static const onboarding1 =
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee";
+
+  static const onboarding2 =
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e";
+
+  static const onboarding3 =
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470";
+}
